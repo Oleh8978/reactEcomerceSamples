49d439b997a66c29d9d73c457b2c248ecb795fc3
Oleh8978 <Oleh8978>
2021-12-22 01:43:03 +0200

    builded files

---
+++ build/precache-manifest.9adb30bd0d7f14002c7095e325201ac7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef8d78e4fc6ac727b22917543c7b69c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e11a7cefca69de0c1be",
+    "url": "/static/css/2.58570433.chunk.css"
+  },
+  {
+    "revision": "018b6502698f84338c42",
+    "url": "/static/css/main.0d2bfa13.chunk.css"
+  },
+  {
+    "revision": "7e11a7cefca69de0c1be",
+    "url": "/static/js/2.f9295739.chunk.js"
+  },
+  {
+    "revision": "018b6502698f84338c42",
+    "url": "/static/js/main.7a46b3e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
