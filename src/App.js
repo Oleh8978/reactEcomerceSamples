c6d8e0890c3d5a7dbbb34d84a081912c6bc29153
Oleh8978 <Oleh8978>
2021-12-22 01:36:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+          Hello world
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
